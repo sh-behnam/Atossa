8260027ad42805bb9fc42c8fa6595e1e0100113c
SajjadPourali <sajjad@securation.com>
2017-12-16 18:16:18 +0330

    Add Basic Files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod webservice;
+mod crawler;
+
+struct Request<'a> {
+    Header: &'a str,
+    Body: &'a str,
+}
+struct Response<'a> {
+    Header: &'a str,
+    Body: &'a str,
+}
+struct HTTP<'a> {
+    Request: Request<'a>,
+    Response: Response<'a>,
+}
+
+fn crawl(HTTP: HTTP,depth: u8) -> HTTP {
+    HTTP // Mina ShahsavanPour
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Scanner!");
 }
