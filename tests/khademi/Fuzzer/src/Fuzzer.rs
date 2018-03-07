5af87aa155446d47a91e9fc9f9926b82c01ef254
Saeed KhademiDoroh <saeed.infact@gmail.com>
2018-03-07 14:07:11 +0330

    Directory Search & Fuzzer Output

--- tests/khademi/Fuzzer/src/Fuzzer.rs
+++ tests/khademi/Fuzzer/src/Fuzzer.rs
@@ -1,2 +1,65 @@
 mod http;
 mod Crawler;
+
+pub enum Vulnerabillity {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+pub enum Option {
+    DIRECTORY_SEARCH,
+    SQL_INJECTION,
+}
+
+pub struct Output {
+    input: String,
+    output: String,
+    http: http
+}
+
+impl Output {
+    pub fn new(
+        input: String,
+        output: u16,
+        http: http,
+    ) -> Output {
+        Output {
+            input: input,
+            output: output,
+            http: http,
+        }
+    }
+}
+
+pub struct Fuzzer {
+    dir_list: Vec<String>,
+    http_list: Vec<http>,
+}
+
+impl Fuzzer {
+    pub fn new(
+        dir_list: Vec<String>
+    ) -> Fuzzer {
+        Fuzzer {
+            dir_list: dir_list,
+        }
+    }
+    pub fn execute(&self, option: Option, http: http) {
+        match option {
+            DIRECTORY_SEARCH => self.dir_search(http),
+        }
+    }
+}
+
+impl Fuzzer {
+    pub fn dir_search(http: http) {
+        http_list: Vec<http>;
+        for dir in dir_list.iter() {
+            _http = http.clone();
+            _http.uri = format!("{}{}", http.uri, dir);
+            _http.do_request();
+            http_list.push(_http);
+        }
+    }
+}
